a4eed177fa2a361c99de2f6e2a67cf5823339225
ericoh1234 <37450746+ericoh1234@users.noreply.github.com>
2025-04-07 00:23:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+import './fileUpload.js';
+
+import { uploadFile } from './fileUpload.js';
+
+function fileUpload() {
+  console.log("fileUpload function called");
+  const fileInput = document.getElementById('fileInput');
+  const file = fileInput.files[0];
+  console.log('file: ', file);
+
+  if (file) {
+        console.log('About to call uploadFile with:', file);
+    uploadFile(file);
+  } else {
+    console.log('No file selected.');
+  }
+}
+
+function main(){
+    console.log('main function called');
+    const uploadButton = document.getElementById('uploadButton');
+    uploadButton.addEventListener('click', fileUpload);
+}
+
+main()
